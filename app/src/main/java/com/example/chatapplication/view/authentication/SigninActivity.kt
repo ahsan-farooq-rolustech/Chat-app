b5162529e01810f57815d61a5f8083a4ae6e0421
Muhammad Ahsan Farooq <ahsan.farooq@rolustech.net>
2022-09-06 14:54:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/chatapplication/view/authentication/SigninActivity.kt
@@ -0,0 +1,14 @@
+package com.example.chatapplication.view.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapplication.R
+
+class SigninActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
