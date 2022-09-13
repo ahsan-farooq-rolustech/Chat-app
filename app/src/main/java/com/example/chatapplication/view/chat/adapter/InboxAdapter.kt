d66b5b813a1ffa68ee51d3ea81e821a5b27d8be6
Muhammad Ahsan Farooq <ahsan.farooq@rolustech.net>
2022-09-13 15:12:15 +0500

    user image in user fragment

--- app/src/main/java/com/example/chatapplication/view/chat/adapter/InboxAdapter.kt
+++ app/src/main/java/com/example/chatapplication/view/chat/adapter/InboxAdapter.kt
@@ -5,12 +5,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapplication.data.responseModel.ChatMessageResponseModel
 import com.example.chatapplication.data.responseModel.UserResponseModel
-import com.example.chatapplication.databinding.AdapterRecentConversationBinding
+import com.example.chatapplication.databinding.AdapterInboxBinding
 import com.example.chatapplication.utilities.utils.IUserListener
 
 class InboxAdapter(private val conversationList: ArrayList<ChatMessageResponseModel>) : RecyclerView.Adapter<InboxAdapter.ConversationViewHolder>()
 {
-    class ConversationViewHolder(val binding: AdapterRecentConversationBinding) : RecyclerView.ViewHolder(binding.root)
+    class ConversationViewHolder(val binding: AdapterInboxBinding) : RecyclerView.ViewHolder(binding.root)
 
     private lateinit var mListener: IUserListener
     fun setListinners(listener: IUserListener)
@@ -20,7 +20,7 @@ class InboxAdapter(private val conversationList: ArrayList<ChatMessageResponseMo
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConversationViewHolder
     {
-        return ConversationViewHolder(AdapterRecentConversationBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return ConversationViewHolder(AdapterInboxBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ConversationViewHolder, position: Int)
